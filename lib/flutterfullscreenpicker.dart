7169726efe2f073e0962418020e5a8ece574c620
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-06-25 21:48:26 +0100

    created interface to access it

--- lib/flutterfullscreenpicker.dart
+++ lib/flutterfullscreenpicker.dart
@@ -8,3 +8,35 @@ import 'package:flutter/material.dart';
 part 'models/select_option.dart';
 part 'src/flutter_fullscreen_picker.dart';
 part 'src/flutter_fullscreen_picker_search.dart';
+
+class FlutterFullScreenPicker {
+  Future openPicker({
+    BuildContext context,
+    List<SelectOption> options,
+    String appBarTitle,
+    Color pageBackgroundColor,
+    Color appBarTitleColor,
+    Color appBarIconsColor,
+    Color optionTextColor,
+  }) async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenPicker(
+          pageTitle: appBarTitle,
+          selectOptions: options,
+          pageBackgroundColor: pageBackgroundColor,
+          appBarTitleColor: appBarTitleColor,
+          appBarIconsColor: appBarIconsColor,
+          optionTextColor: optionTextColor,
+        ),
+        fullscreenDialog: false,
+      ),
+    ).then((value) {
+      if (value != null) {
+        return value;
+      }
+      return null;
+    });
+  }
+}
